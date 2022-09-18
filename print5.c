70cd6b60d090ec7cdb795a6c0b20d43bf96a7fb1
bigboytimi <olutimiolowookere@icloud.com>
2022-09-18 11:15:01 +0000

    Use a for loop to print hello world 5 times

---
+++ print5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry program
+ *
+ * Description: Print Hello world 5 times
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 5; i++)
+	{
+		printf("Hello, World\n");
+	}
+}
